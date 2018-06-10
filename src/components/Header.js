3fb79f7e26034eb2fffb71e172124d55508d74b9
Scott <david.bowen@interapthq.com>
2018-06-10 08:48:19 -0400

    created Header component & reference to it in index.js

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+//Import a libraries for making a component
+import React from 'react';
+import { Text } from 'react-native';
+
+//Create a component
+const Header = () => {
+  return <Text>75˚</Text>;
+};
+
+//make component available to other parts of the app
+export default Header;
