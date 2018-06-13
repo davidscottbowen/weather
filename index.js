78122450a69bf88374f829750d7404b8530f93e6
Scott <david.bowen@interapthq.com>
2018-06-13 19:32:52 -0400

    created weatherlist component & exported to app root

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 //Import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
+import WeatherList from './src/components/WeatherList';
 
 //Create a component
 const App = () => (
-    <Header headerText={'Paintsville, KY'} />
+    <View>
+      <Header headerText={'Paintsville, KY'} />
+      <WeatherList />;
+    </View>
   );
 
 //Render it to the device
---
+++ src/components/WeatherList.js
@@ -0,0 +1,15 @@
+//Import a libraries for making a component
+import React from 'react';
+import { Text, View } from 'react-native';
+
+//Create a component
+const WeatherList = () => {
+  return (
+    <View>
+      <Text>WeatherList!!!!</Text>
+    </View>
+  );
+};
+
+//make component available to other parts of the app
+export default WeatherList;
